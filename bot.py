f46ad1a626cdca4ac688a92bbaee826a72912abd
am303071 <107723631+am303071@users.noreply.github.com>
2026-09-06 05:52:32 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
 
+import os
+import requests
+
+TOKEN = os.getenv("BALE_TOKEN")
+CHAT_ID = os.getenv("BALE_CHAT_ID")
+
+def send_message(text):
+    url = f"https://tapi.bale.ai/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+
+    response = requests.post(url, json=data)
+
+    print(response.status_code)
+    print(response.text)
+
+
+send_message("🔔 باید سه‌شنبه نامه ریکال رو بزنی")
